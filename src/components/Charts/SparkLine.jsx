75de3ead6cee42ef87abd817cbafeb4edc2f1fff
Julio Esteban Cáceres Sanzana <Julio@Macbook-M1-Pro.local>
2024-05-21 16:25:05 -0400

    Avances en el tutorial

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,7 +1,49 @@
-import React from "react"
+import React from "react";
+import {
+	SparklineComponent,
+	Inject,
+	SparklineTooltip,
+} from "@syncfusion/ej2-react-charts";
 
-const SparkLine = () => {
-    return <div>SparkLine</div>
+// SyncFusion posee una buena documentacion, en caso de cualquier duda,
+// lo ideal es visitar su web oficial
+
+class SparkLine extends React.PureComponent {
+	render() {
+		const { id, height, width, color, data, type, currentColor } =
+			this.props;
+
+		return (
+			<SparklineComponent
+				id={id}
+				height={height}
+				width={width}
+				lineWidth={1}
+				valueType="Numeric"
+				fill={color}
+				border={{ color: currentColor, width: 2 }}
+				tooltipSettings={{
+					visible: true,
+					// eslint-disable-next-line no-template-curly-in-string
+					format: "${x} : data ${yval}",
+					trackLineSettings: {
+						visible: true,
+					},
+				}}
+				markerSettings={{
+					visible: ["All"],
+					size: 2.5,
+					fill: currentColor,
+				}}
+				dataSource={data}
+				xName="x"
+				yName="yval"
+				type={type}
+			>
+				<Inject services={[SparklineTooltip]} />
+			</SparklineComponent>
+		);
+	}
 }
 
-export default SparkLine
+export default SparkLine;
